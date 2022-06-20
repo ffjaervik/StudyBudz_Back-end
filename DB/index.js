fec04173d592cee805971170e6f7d4166dbb319d
jadenjoel <mailtojadenjoel76@gmail.com>
2022-06-20 23:30:26 +0100

    Added some folders from our plan and imported pg module to comm with our DB

---
+++ DB/index.js
@@ -0,0 +1,27 @@
+//Import pg module to communicate with our database
+import pg from "pg";
+
+//Setup a new pool which will be the connection to the database
+//Use our Heroku credentials in the pool or client
+const pool = new pg.Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+
+//Make a function that takes in an SQL string and runs pool.query using that string
+    //Export query function 
+export function poolQuery(string) {
+    return pool.query(string)
+}
+//Testing that the pool query works and what it gives back.
+async function log() {
+    let now = await poolQuery(`SELECT NOW()`)
+    console.log(now)
+}
+// log();
