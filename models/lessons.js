2e5a6cc70449801bc49a657fafe9e4512ffbee0c
enantes <sambaeverything@gmail.com>
2022-06-21 16:47:11 +0200

    added version number to package.json

--- models/lessons.js
+++ models/lessons.js
@@ -12,3 +12,14 @@ export const selectLessonsById = async function (id) {
   console.log(results.rows);
   return results.rows;
 };
+
+async function populateDogsTable() {
+  for (let i = 0; i < dogsData.length; i++) {
+       const res = await query (
+           `INSERT INTO dogs (dog_name, dog_gender, owner_name, email) VALUES ($1, $2, $3, $4) RETURNING *;`, 
+           [dogsData[i].dog_name, dogsData[i].dog_gender, dogsData[i].owner_name, dogsData[i].email]
+       );
+   console.log(res.rows[0],"inserted");
+   }
+}
+
