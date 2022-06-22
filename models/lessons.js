8da73de90c6a0f9500d95aeb84a26369600cbec4
enantes <sambaeverything@gmail.com>
2022-06-22 12:30:59 +0200

    lower case stuff

--- models/lessons.js
+++ models/lessons.js
@@ -15,7 +15,7 @@ export const selectLessonsById = async function (id) {
 
 const sqlString = `CREATE TABLE IF NOT EXISTS lessons (lesson_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name TEXT, topic TEXT, description TEXT, zoom TEXT, paypalEmail TEXT, dateTime timestamp, duration INT, starRating INT);`;
 
-export const postLessons = async function postLessons(name, topic, description, zoom, paypalEmail, dateTime, duration, starRating) {
+export const postLessons = async function postLessons(name, topic, description, zoom, paypalemail, datetime, duration, starrating) {
        const results = await pool.query(
            `INSERT INTO lessons (name, topic, description, zoom, paypalemail, datetime, duration, starrating) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`, 
            [name, topic, description, zoom, paypalemail, datetime, duration, starrating]
