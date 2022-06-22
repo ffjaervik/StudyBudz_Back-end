a7169e53b7298069fffb5c43cffc0f710e5779ed
Kieran <kieran.webdevelopment@gmail.com>
2022-06-22 15:51:46 +0100

    datase date changed

--- routes/lessons.js
+++ routes/lessons.js
@@ -1,35 +1,46 @@
-import express from "express";
-import { selectAllLessons, selectLessonsById, postLessons, selectLessonsByTopic } from "../models/lessons.js";
+import express from 'express';
+import {
+  selectAllLessons,
+  selectLessonsById,
+  postLessons,
+  selectLessonsByTopic,
+} from '../models/lessons.js';
 const router = express.Router();
 
 // http://localhost:3000/lessons/q?topic=html
-router.get("/q", async (req, res) => { //How to have two .get/ without conflict?
-  const topic = req.query.topic
-  console.log('this is topic ', topic)
+router.get('/q', async (req, res) => {
+  //How to have two .get/ without conflict?
+  const topic = req.query.topic;
+  console.log('this is topic ', topic);
   const results = await selectLessonsByTopic(topic);
   res.json({ success: true, payload: results });
 });
 
-
-router.get("/", async (req, res) => {
-
+router.get('/', async (req, res) => {
   const results = await selectAllLessons();
   res.json({ success: true, payload: results });
 });
 
-router.get("/:id", async (req, res) => {
+router.get('/:id', async (req, res) => {
   const results = await selectLessonsById(req.params.id);
   res.json({ success: true, payload: results });
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const data = req.body;
-  const results = await postLessons(data.name, data.topic, data.description, data.zoom, data.paypalemail, data.datetime, data.duration, data.starrating);
+  console.log(data);
+  const duration = 30;
+  const results = await postLessons(
+    data.name,
+    data.topic,
+    data.description,
+    data.zoom,
+    data.paypalemail,
+    data.datetime,
+    duration,
+    data.starrating
+  );
   res.json({ success: true, payload: results });
 });
 
- 
- 
-
- 
 export default router;
