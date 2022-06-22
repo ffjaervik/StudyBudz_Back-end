b0c49ef4e7b456eb82826a08ee3c94334410070e
jadenjoel <mailtojadenjoel76@gmail.com>
2022-06-22 14:03:31 +0100

    cors added

--- app.js
+++ app.js
@@ -1,35 +1,40 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 
-import __dirname  from './dirname.js';
-import cookieParser  from 'cookie-parser';
-import cors  from 'cors';
-import logger  from 'morgan';
+import __dirname from "./dirname.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import logger from "morgan";
 
-import router from './routes/lessons.js';
+import router from "./routes/lessons.js";
 
 const app = express();
 
-app.use(logger('dev'));
-app.use(cors());
+app.use(logger("dev"));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.get("/",(req,res)=>{
-res.json({message:"This is me"})
-})
+app.get("/", (req, res) => {
+  res.json({ message: "This is me" });
+});
 
-app.use('/lessons', router);
-
+app.use("/lessons", router);
 
 app.use(function (req, res, next) {
-  res.status(404).json({message: "We couldn't find what you were looking for 😞"})
-})
+  res
+    .status(404)
+    .json({ message: "We couldn't find what you were looking for 😞" });
+});
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).json(err)
-})
+  console.error(err.stack);
+  res.status(500).json(err);
+});
 
 export default app;
